54a0a61b57a6bb5351c47472eaea79a1fd75c18e
小沼智則 <omatomemail0718@gmail.com>
2021-04-21 17:19:55 +0900

    仮バリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   def index
   end
   
@@ -8,5 +9,11 @@ class ItemsController < ApplicationController
 
   def create 
 
+  end
+
+    def move_to_index
+     unless user_signed_in?
+     redirect_to action: :index
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  with_options presence: true do
+   validates :image
+   validates :title 
+   validates :description
+   validates :category_id
+   validates :status_id
+   validates :shipping_id
+   validates :area_id
+   validates :days_id
+   validates :price
+  end
 end
