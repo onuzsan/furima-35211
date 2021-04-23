214ba761c863fb6a3625b425e590941fb8c892d2
小沼智則 <omatomemail0718@gmail.com>
2021-04-23 13:05:57 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,9 +21,12 @@ class Item < ApplicationRecord
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is invalid' }
     validates :price, format: { with: /\A[0-9]+\z/ }
   end
-  validates :category_id, numericality: { other_than: 1 }
-  validates :status_id,   numericality: { other_than: 1 }
-  validates :shipping_id, numericality: { other_than: 1 }
-  validates :area_id,     numericality: { other_than: 1 }
-  validates :schedule_id, numericality: { other_than: 1 }
+  
+  with_options  numericality: { other_than: 1 } do
+  validates :category_id
+  validates :status_id
+  validates :shipping_id
+  validates :area_id
+  validates :schedule_id
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -30,33 +30,73 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank", 'Category is not a number')
     end
+    it 'カテゴリーは１が選択された場合は出品できないこと' do
+      @item.category_id ='1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
     it '商品の状態についての情報が必須であること' do
       @item.status_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Status can't be blank", 'Status is not a number')
+    end
+    it '商品の状態は１が選択された場合は出品できないこと' do
+      @item.status_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status must be other than 1")
     end
     it '配送料の負担についての情報が必須であること' do
       @item.shipping_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping can't be blank", 'Shipping is not a number')
     end
+    it '配送料の負担は１が選択された場合は出品できないこと' do
+      @item.shipping_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping must be other than 1")
+    end
     it '発送元の地域についての情報が必須であること' do
       @item.area_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Area can't be blank", 'Area is not a number')
+    end
+    it '発送元の地域は１が選択された場合は出品できないこと' do
+      @item.area_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Area must be other than 1")
     end
     it '発送までの日数についての情報が必須であること' do
       @item.schedule_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Schedule can't be blank", 'Schedule is not a number')
     end
+    it '発送までの日数は１が選択された場合は出品できないこと' do
+      @item.schedule_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Schedule must be other than 1")
+    end
     it '販売価格についての情報が必須であること' do
       @item.price, = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is invalid', "Price can't be blank", 'Price is invalid')
     end
+    it '商品価格が半角英数字混合では出品できない' do
+      @item.price, = 'a111'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is invalid")
+    end
+    it '商品価格が半角英字のみでは出品できない' do
+      @item.price, = 'asde'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is invalid")
+    end
     it '販売価格は、¥300~¥9,999,999の間のみ保存可能であること' do
       @item.price, = '100'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price is invalid')
+    end
+    it '商品価格が10,000,000以上では登録できないこと' do
+      @item.price, = '10000000000000'
       @item.valid?
       expect(@item.errors.full_messages).to include('Price is invalid')
     end
