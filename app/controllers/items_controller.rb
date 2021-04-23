87626380d5fc5004cbea91f0004e7f920df240ef
小沼智則 <omatomemail0718@gmail.com>
2021-04-23 12:07:23 +0900

    JSを用いた金額算出機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,34 +2,24 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   def index
   end
-  
+
   def new
-     @item = Item.new
+    @item = Item.new
   end
 
-  def create 
-      @item = Item.new(item_params)
+  def create
+    @item = Item.new(item_params)
     if
      @item .save
-     redirect_to root_path
-    else 
+      redirect_to root_path
+    else
       render :new
     end
   end
 
-
-
-
-
-
-
-
   private
-  def item_params
-    params.require(:item).permit(:image,:title,:description, :category_id,:status_id,:shipping_id,:area_id,:schedule_id,:price,).merge(user_id: current_user.id)
-  end
 
-    
-   
-  
+  def item_params
+    params.require(:item).permit(:image, :title, :description, :category_id, :status_id, :shipping_id, :area_id, :schedule_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,23 +9,21 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-
   with_options presence: true do
-   validates :image
-   validates :title 
-   validates :description
-   validates :category_id
-   validates :status_id
-   validates :shipping_id
-   validates :area_id
-   validates :schedule_id
-   validates :price,numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
-   validates :price,format: { with: /\A[0-9]+\z/}
+    validates :image
+    validates :title
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :shipping_id
+    validates :area_id
+    validates :schedule_id
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is invalid' }
+    validates :price, format: { with: /\A[0-9]+\z/ }
   end
   validates :category_id, numericality: { other_than: 1 }
   validates :status_id,   numericality: { other_than: 1 }
   validates :shipping_id, numericality: { other_than: 1 }
   validates :area_id,     numericality: { other_than: 1 }
   validates :schedule_id, numericality: { other_than: 1 }
-  
 end
