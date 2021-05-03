f7171665ef5a00987a5717f5d43826d7d36d0743
小沼智則 <omatomemail0718@gmail.com>
2021-05-03 21:16:30 +0900

    ソールドアウト条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,7 @@ class ItemsController < ApplicationController
   before_action :set_root, only: [:edit,:update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
+    
   end
 
   def new
